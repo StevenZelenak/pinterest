3a82ad6838aa1954381c52e9c251392034638483
Steven Zelenak <stevenmzelenak@gmail.com>
2021-04-26 16:47:09 -0500

    update

--- src/javascripts/components/homePage/home.js
+++ src/javascripts/components/homePage/home.js
@@ -2,7 +2,7 @@ import utils from '../../helpers/utils';
 
 const HomePage = () => {
   let domString = '';
-  domString = '<h1>Pintrest</h1>';
+  domString = '<h1>Pinterest</h1>';
   utils.printToDom('pint-home', domString);
 };
 
